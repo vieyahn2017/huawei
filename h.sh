2bc8e85a790e84540ab89db82cc1cc6464c761f7
yanghao <283403891@qq.com>
2018-04-20 23:42:50 +0800

    push.sh add

--- h.sh
+++
@@ -1,8 +0,0 @@
-git add .
-
-git commit -m "$1"
-
-if [ "$2"]; then
-        echo "$2"
-	# git push -u origin master
-fi
--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ git add .
 
 git commit -m "$1"
 
-if [ "$2"]; then
+if [ "$2" ]; then
         echo "$2"
 	# git push -u origin master
 fi
